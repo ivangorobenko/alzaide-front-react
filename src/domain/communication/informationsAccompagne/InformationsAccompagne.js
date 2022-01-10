fc9f7c16976113cf1f5aee9f76b6f1a0aef3c191
ivan.gorobenko <igorobenko@digikare.com>
2022-01-10 10:17:42 +0100

    feat: améliorer visu informations accompagne

--- src/domain/communication/informationsAccompagne/InformationsAccompagne.js
+++ src/domain/communication/informationsAccompagne/InformationsAccompagne.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 import {Message} from "../message/Message";
-import {Button, Grid, Typography, useTheme} from "@material-ui/core";
+import {Button, Grid, Paper, Typography, useTheme} from "@material-ui/core";
 import Header from "./headerAccompagne";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import moment from "moment";
@@ -15,12 +15,18 @@ const useStyles = makeStyles(() =>
             alignItems: "center",
             justifyContent: "flex-start",
         },
-        containerZonePrincipale: {
+        zonePrincipale: {
             height: "70vh"
+        },
+        informations: {height: "80%"
         },
         titreMessages: {
             color: "grey",
             padding: "16px 0 0 16px"
+        },
+        messages: {
+            height: "100%",
+            overflowY: "scroll"
         },
         buttonContainer: {
             width: "100%",
@@ -29,6 +35,15 @@ const useStyles = makeStyles(() =>
         },
         button: {
             height: "100%"
+        },
+        colonneDroite: {
+            justifyContent: "center",
+            textAlign: "center",
+            marginTop: "30px"
+        },
+        placeHolder: {
+            height: "100%",
+            color: "grey"
         }
 
     }));
@@ -44,13 +59,13 @@ export const InformationsAccompagne = ({messages, alerte, getMessages, alerterAc
     return (
         <div className={classes.container}>
             <Header/>
-            <Grid className={classes.containerZonePrincipale}
+            <Grid className={classes.zonePrincipale}
                   container
                   direction="column"
                   justifyContent="space-between"
                   alignItems="center">
-                <Grid container item direction={"row"}>
-                    <Grid item xs={12}>
+                <Grid container className={classes.informations} item direction={"row"}>
+                    <Grid className={classes.messages}  item xs={8}>
                         <div>
                             <Typography className={classes.titreMessages} variant={"h5"}>Messages de
                                 Tatiana</Typography>
@@ -59,11 +74,14 @@ export const InformationsAccompagne = ({messages, alerte, getMessages, alerterAc
                             }
                         </div>
                     </Grid>
-                    <Grid item xs={0}>
+                    <Grid className={classes.colonneDroite} item xs={4}>
+                        <Paper className={classes.placeHolder}>Plus de fonctionnalités ici</Paper>
+
                     </Grid>
                 </Grid>
                 <Grid className={classes.buttonContainer} item>
-                    <Button className={classes.button} fullWidth={true} disabled={alerte} variant={"contained"}
+                    <Button className={classes.button} fullWidth={true} disabled={alerte !== undefined}
+                            variant={"contained"}
                             color={"secondary"} size={"large"}
                             onClick={alerterAccompagnant}>{alerte ? "Message d'urgence envoyé à " + moment(alerte.timestamp).format("LT") : "ENVOYER UN MESSAGE D'URGENCE"}</Button>
                 </Grid>
