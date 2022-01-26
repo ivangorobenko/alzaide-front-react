53ce2a960d06a3b9bd0c28a551796d66e9cacded
ivangorobenko <49490325+ivangorobenko@users.noreply.github.com>
2022-01-26 16:52:19 +0100

    feat: ne pas utiliser redux plugin en prod

--- src/store/initStore.js
+++ src/store/initStore.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import thunk from "redux-thunk";
 import {axiosHttpClient} from "./dependencies/axiosHttpClient";
 import {nativeTimer} from "./dependencies/nativeTimer";
@@ -11,7 +11,8 @@ const initialiseDependencies = () => ({
     geoLocationService: browserGeolocationService
 });
 export const initStore = () => {
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ;
+    const composeEnhancers =
+        process.env.NODE_ENV === "production" ? compose : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     return createStore(
         rootReducer,
         composeEnhancers(applyMiddleware(thunk.withExtraArgument({...initialiseDependencies()})))
